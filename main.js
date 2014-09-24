1939a35b55bd7712a2c1ff3cfb0a59e9c5ae0493
Michael Makarov <info1117@gmail.com>
2014-09-23 20:39:11 -0700

    Cats can make kids

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 angular.module("randomApp", [])
 .directive("packOfBeer", function () {
 	return {
-		template: "<div><h1>This is a pack of beer with a hiding cat {{catName}} in it</h1><p><===|</p><p><===|</p><p>@_@</p><p><===|</p><p><===|</p><button ng-click='killCat(catName)'>Kill kitty</button></div>",
-		scope: {catName: '=', killCat: '&'}
+		controller: "packOfBeerCtrl",
+		template: "<div><h1>This is a pack of beer with a hiding cat {{catName}} in it</h1><p><===|</p><p><===|</p><p>@_@</p><p><===|</p><p><===|</p><button ng-click='killCat(catName)'>Kill kitty</button><button ng-click='makeCat({name: kidName()})'>Make a kid</button></div>",
+		scope: {catName: '=', killCat: '&', makeCat: '&'}
 	};
 })
+.controller('packOfBeerCtrl', ['$scope', function($scope) {
+	$scope.kidName = function () {
+		return $scope.catName + "'s kid";
+	}
+}])
 .controller('WarehouseCtrl', ['$scope', function($scope) {
 	$scope.cats = ["Morris", "Felix", "Josh", "Billy"];
 	$scope.killCat = function(cat) {
@@ -13,5 +19,9 @@ angular.module("randomApp", [])
 			$scope.cats.splice(index, 1);
 		};
 		alert("Good bye, " + cat);
+	}
+	$scope.makeCat = function(cat) {
+		$scope.cats.push(cat);
+		alert("Welcome, " + cat);
 	}
 }]);
