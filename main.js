47aa235ab4c2d9128f042af2b65df2713a31c6b1
Michael Makarov <info1117@gmail.com>
2014-09-23 19:55:01 -0700

    Adds cat names

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 angular.module("randomApp", [])
 .directive("packOfBeer", function () {
 	return {
-		template: "<div><h1>This is a pack of beer with a hiding cat in it</h1><p><===|</p><p><===|</p><p>@_@</p><p><===|</p><p><===|</p></div>"
+		template: "<div><h1>This is a pack of beer with a hiding cat {{catName}} in it</h1><p><===|</p><p><===|</p><p>@_@</p><p><===|</p><p><===|</p></div>",
+		scope: {catName: '@'}
 	};
 });
