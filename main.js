e4ea791489123fbe3658650b4b2c1edf3bc93d2a
Michael Makarov <info1117@gmail.com>
2014-09-23 20:15:13 -0700

    Loop through packs of beers

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ angular.module("randomApp", [])
 .directive("packOfBeer", function () {
 	return {
 		template: "<div><h1>This is a pack of beer with a hiding cat {{catName}} in it</h1><p><===|</p><p><===|</p><p>@_@</p><p><===|</p><p><===|</p></div>",
-		scope: {catName: '@'}
+		scope: {catName: '='}
 	};
-});
+})
+.controller('WarehouseCtrl', ['$scope', function($scope) {
+	$scope.cats = ["Morris", "Felix", "Josh", "Billy"];
+}]);
